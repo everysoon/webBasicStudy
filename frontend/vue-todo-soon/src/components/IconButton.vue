f871b1677ea9c60c0211ce7b946a8d1e040d1690
everysoon <minsoon1269@naver.com>
2021-08-05 11:44:35 +0900

    vue 공부 upload

---
+++ frontend/vue-todo-soon/src/components/IconButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-btn icon>
+    <v-icon>
+      {{ icon }}
+    </v-icon>
+  </v-btn>
+</template>
+<script>
+export default {
+  props: {
+    icon: String,
+  },
+};
+</script>
--- frontend/vue-todo-soon/src/views/Home.vue
+++ frontend/vue-todo-soon/src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <h1>Soon Todo List</h1>
+  <v-app-bar>
+    <v-app-bar-nav-icon />
+    <v-app-bar-title>Title</v-app-bar-title>
+    <v-spacer />
+    <icon-button icon="mdi-account-plus" />
+    <icon-button icon="mdi-login" />
+  </v-app-bar>
   <TodoList />
   <TodoInput />
 </template>
 
 <script>
-import TodoList from "@/components/TodoList";
-import TodoInput from "@/components/TodoInput";
+import IconButton from '@/components/IconButton';
+import TodoList from '@/components/TodoList';
+import TodoInput from '@/components/TodoInput';
 export default {
-  name: "Home",
+  name: 'Home',
 
   components: {
     TodoList,
-    TodoInput
-  }
+    TodoInput,
+    IconButton,
+  },
 };
 </script>
